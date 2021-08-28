1a8829b0cee932e32091e7571607c8e3ab1fc797
Michael Ellis <michael.f.ellis@gmail.com>
2021-08-28 16:28:42 -0400

    Add initial version of ghotify command line utility

---
+++ cmd/gohtify/main.go
@@ -0,0 +1,29 @@
+// gohtify reads html fragments from stdin and emits goht code on stdout.
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/Michael-F-Ellis/goht"
+)
+
+func main() {
+	ignoreTags := map[string]struct{}{
+		"html": {}, "head": {}, "body": {},
+	}
+	html, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	var gohtText string
+	err = goht.Gohtify(string(html), ignoreTags, &gohtText)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(gohtText)
+	os.Exit(0)
+}
